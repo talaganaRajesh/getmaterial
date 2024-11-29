3cb722a704cc270f1da956ce036c01f60d66f560
Rajesh <talaganarajesh@gmail.com>
2024-11-29 10:42:54 +0530

    asdkfjsdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,36 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+  
+// // Add this line
+// server: {
+//   historyApiFallback: true
+// }
+
+
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-// Add this line
-server: {
-  historyApiFallback: true
-}
-
-
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
