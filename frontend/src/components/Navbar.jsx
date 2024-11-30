d00193c3c724ec6e18921c6ab0f05320265e4b66
Rajesh <talaganarajesh@gmail.com>
2024-11-30 22:24:57 +0530

    done mobile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,16 +38,16 @@ function Navbar({ user }) {
         <div>
           {user ? (
             <>
-              <Link to="/upload" className="text-black bg-white py-2 px-5 border-white border-4 rounded-3xl mr-4 font-semibold hover:rounded-xl transition-all duration-300">
+              <Link to="/upload" className="text-black md:text-base bg-white md:py-2 md:px-5 text-xs px-1 mr-2 border-white border-4 rounded-3xl md:mr-4 font-semibold hover:rounded-xl transition-all duration-300">
                 Upload
               </Link>
-              <button onClick={handleSignOut} className="text-white bg-black opacity-80 font-semibold py-2 px-4 rounded-lg hover:text-red-500 transition-colors duration-300">
+              <button onClick={handleSignOut} className="text-white md:text-base bg-black opacity-80 text-xs font-semibold md:py-2 md:px-4 rounded-lg hover:text-red-500 transition-colors duration-300">
                 Sign Out
               </button>
             </>
           ) : (
             <div className='flex justify-center'>
-              <Link to="/auth" className="text-black font-bold bg-gradient-to-r from-cyan-400 to-green-500 py-3 px-4 rounded-lg hover:underline">
+              <Link to="/auth" className="text-black font-bold text-sm w-1/2 text-center md:w-full md:text-base bg-gradient-to-r from-cyan-400 to-green-500 md:py-3 md:px-4 rounded-lg hover:underline">
                 Become a Contributor
               </Link>
 
